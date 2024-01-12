cfb31627322fdac0a39c5388a7cd1ec13a54c259
Hassan Javed <hassan.javed@funavry.com>
2024-01-12 16:57:46 +0500

    clock

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'test-project';
+  second_rotate = 0
+  minute_rotate = 0
+  hour_rotate = 0
+  ngOnInit(): void {
+    //second manipulation
+    setInterval(() =>
+    {
+      this.second_rotate += 6
+    },1000)
+    setInterval(() =>
+    {
+      this.minute_rotate += 6
+    },60000)
+    setInterval(() =>
+    {
+      this.hour_rotate += 6
+    },540000)
+  }
 }
